fdc8795a3a6db2a2585e47eefbb15b28ec56eb75
James Hart <james.elliot.hart@gmail.com>
2020-10-17 16:07:32 -0500

    add mediatr

--- ExampleMediatR/Startup.cs
+++ ExampleMediatR/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MediatR;
 
 namespace ExampleMediatR
 {
@@ -26,6 +27,7 @@ namespace ExampleMediatR
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
